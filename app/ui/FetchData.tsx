4d72cf6f91e64dee8e039f9e7a9561c7258fd043
takahara tomoya <t.tkorock221@gmail.com>
2024-07-13 23:12:40 +0900

    一旦

--- app/ui/FetchData.tsx
+++ app/ui/FetchData.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import { useQuery } from '@tanstack/react-query'
-import type {UseQueryResult} from '@tanstack/react-query'
 import type { Query } from '@/types/prismicio-types'
+import { pages } from 'next/dist/build/templates/app-page'
 
 const fetchAllPage = async () => {
     try {
@@ -14,9 +14,30 @@ const fetchAllPage = async () => {
     }
 }
 
+const initialResponse:Query = {
+    allPages: {
+        edges: [
+            {
+                node: {}
+            }
+        ],
+        pageInfo: {
+            hasNextPage: false,
+            hasPreviousPage: false
+        },
+        totalCount: 1
+    }
+}
+
 export const FetchData = () => {
-    const { data }: UseQueryResult<Query> = useQuery({queryKey: ['pages'], queryFn: fetchAllPage})
-    const pageArr = data?.allPages?.edges.map(pageData =>
+    const { data, isLoading, error } = useQuery<Query>({
+        queryKey: ['pages'],
+        queryFn: fetchAllPage,
+        initialData: initialResponse
+    })
+    if(!data.allPages.edges) {return <div>エラー</div>　}
+    // if(!data.allPages.edges || !data) {return <div>エラー</div>　}
+    const pageArr = data.allPages.edges.map(pageData =>
         <div key={pageData.node._meta.id}>{pageData.node.title[0].text}</div>
     );
 
