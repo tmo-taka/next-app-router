5199db14b3597ef6193ff9e769b8e522beb46725
takahara tomoya <t.tkorock221@gmail.com>
2024-08-02 23:30:10 +0900

    tailwind少し対応

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function Layout({children}: Props) {
     return (
         // NOTE: suppressHydrationWarning(https://financial-programmer.net/blog/nextjs-warning-extra-attributes)
-        <html lang="en" suppressHydrationWarning>
+        <html lang="ja" suppressHydrationWarning>
             <body className="relative" suppressHydrationWarning>
                 <div>{children}</div>
             </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      font: '#3f3f46',
+      accent: '#22c55e',
+      base: '#e0e7ff'
+    }
   },
   plugins: [],
 };
